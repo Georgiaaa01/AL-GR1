22d6c78843f51ace0f75281f0c6510786ec0f2f0
Georgiana Stancu <152000986+Georgiaaa01@users.noreply.github.com>
2025-11-10 11:55:18 +0200

    testare exercitiu

---
+++ testare.js
@@ -0,0 +1,41 @@
+// var var1= 1;
+// let var2 = 2;
+// const var3 = 3;
+
+// var1=5;
+// console.log(var1);
+
+// var2=5;
+// console.log(var2);
+
+// var3=5;
+// console.log(var3);
+
+
+
+function testVariables(param) {
+    console.log(var1);
+    if (param === true) {
+        var var1 = 1;
+        let var2 = 2;
+        const var3 = 3;
+    }
+    console.log(var1);
+    console.log(var2);
+    console.log(var3);
+}
+
+// testVariables(true);
+const testVariables2 = (param) => {
+    console.log(var1);
+    if (param === true) {
+        var var1 = 1;
+        let var2 = 2;
+        const var3 = 3;
+    }
+    console.log(var1);
+    console.log(var2);
+    console.log(var3);
+};
+
+testVariables2(true);
